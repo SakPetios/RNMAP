e5bc683a4852c476850ad9a06935e49d9edd9d34
Antonis Petsos <petsosantonios@gmail.com>
2023-09-25 23:34:58 +0300

    Basic Port Scanner Setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-mod portscan;
+mod ping;
 mod util;
+mod portscan;
 use util::{get_gateway_ip,get_mac_with_ip};
 use pnet::datalink;
-use portscan::{ICMPv4Ping,ICMPv6Ping};
+use ping::{ICMPv4Ping,ICMPv6Ping};
 fn main() {
     println!("{:?}",get_mac_with_ip(&get_gateway_ip().unwrap()));
-    let mut scanner = ICMPv6Ping::new("2606:2800:220:1:248:1893:25c8:1946".parse().unwrap());
-    scanner.ping();
+    let mut scanner = ICMPv6Ping::new();
+    scanner.ping("2606:2800:220:1:248:1893:25c8:1946".parse().unwrap());
 }
